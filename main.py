0cc28720dab58fcfed05c6f08799699481979c8e
Jun Choi <cjy13753@gmail.com>
2021-01-11 12:02:53 +0900

    Add a new data column for calculated value of moisture(g) in one cubic meter

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 import pymysql
 import os
+import pandas as pd
 
 list_hum = []
 list_temp = []
@@ -19,13 +20,14 @@ for i in range(5):
 avg_hum = sum(list_hum)/len(list_hum)
 avg_temp = sum(list_temp)/len(list_temp)
 
-# format = "%Y-%m-%d %H-%M-%S %Z"
-# print("time:{}, temperature: {:.1f}, humidity: {:.1f}".format(datetime.now(timezone('Asia/Seoul')).strftime(format),avg_temp, avg_hum))
+moisture_table = pd.read_csv('~/Desktop/Projects/Hygrometer/temp_moisture.csv', header=0)
+mois_max_in_m3 = list(moisture_table.loc[moisture_table['temperature']==avg_temp, 'max_moisture'])[0]
+mois_real_in_m3 = round(mois_max_in_m3 * avg_hum / 100, 1)
 
 conn = pymysql.connect(host=os.environ.get('AWS_RDS_MYSQL_HOST'), user='admin', password=os.environ.get('AWS_RDS_MYSQL_PASSWORD'), db='humidity')
 cur = conn.cursor()
-query = 'INSERT INTO room_condition VALUES (%s, %s, %s)'
-cur.execute(query, (datetime.now(), avg_temp, avg_hum))
+query = 'INSERT INTO room_condition VALUES (%s, %s, %s, %s)'
+cur.execute(query, (datetime.now(), avg_temp, avg_hum, mois_real_in_m3))
 conn.commit()
 cur.close()
 conn.close()
